b4c1e4cfaaeaef5c6cbd87d99480ccc11a65e107
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-10-26 11:44:53 +0200

    Update index.jsx
    corrected two typos

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // importstatement to indicate that you need to bundle `./index.scss`
-import `./index.scss`;
+import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
@@ -18,4 +18,5 @@ class MyFlixApplication extends React.Component {
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 // Tells React to render your app in the root DOM element
-ReactDOM.render(react.createElement(MyFlixApplication), container);
+ReactDOM.render(React.createElement(MyFlixApplication), container);
+
