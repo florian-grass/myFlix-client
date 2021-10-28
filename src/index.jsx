fc2a9a1142a3c14f50a67eeac7a2bb5facacb038
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-10-28 08:39:47 +0200

    Update index.jsx
    first update for exercise 3.3

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import the MainView component
+import MainView from './components/main-view/main-view';
+
 // importstatement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
@@ -8,15 +11,15 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good Morning</div>
-      </div>
+      <MainView />
     );
   }
 }
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
+
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
+
 
