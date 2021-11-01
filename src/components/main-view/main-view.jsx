baad8d15b154b07c15ba370f5a4b12a3e86388bb
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-11-01 16:27:00 +0100

    Update main-view.jsx
    exercise 3.4
    import axios
    empty movie array
    componentDidMount lifecycle included

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -8,13 +9,7 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: './img/Inception.png', Genre: 'Action, Adventure, Sci-Fi', Director: 'Christopher Nolan' },
-
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/', Genre: 'Drama', Director: 'Frank Darabont' },
-
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: '../img/Gladiator.png', Genre: 'Action, Adventure, Drama', Director: 'Ridley Scott' }
-      ],
+      movies: [],
       selectedMovie: null
     };
   }
