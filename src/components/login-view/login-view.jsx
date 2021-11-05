1f12cbf506155dfd71f2038f1b3f59822902d008
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-11-05 19:17:32 +0100

    Update login-view.jsx
    added bootstrap
    added prop types

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { Container, Row, Col, CardGroup, Card, Form, Button } from 'react-bootstrap';
+
+import './registration-view.scss';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -8,25 +11,70 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedin(username) */
-    props.onLoggedIn(username);
+    axios.post('https://stark-chamber-97082.herokuapp.com/', {
+      Username: username,
+      Password: password,
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open("/" , "_self");
+        // The second argument "_self" is necessary, so that
+        // the page will open in the current page
+      })
+      .catch(e => {
+        console.log('error registering the user');
+        alert('sSomething wasn\'t entered right');
+      })
   };
 
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="enter eMail" />
-      </Form.Group>
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card>
+              <Card.Body>
+                <Card.Title>Please Login</Card.Title>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control 
+                      type="text" 
+                      value={text}
+                      onChange={e => setUsername(e.target.value)} 
+                      placeholder="enter eMail"
+                      required
+                    />
+                  </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="enter password" />
-      </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control 
+                      type="password" 
+                      value={password}
+                      onChange={e => setPassword(e.target.value)} 
+                      placeholder="enter password"
+                      required
+                    />
+                  </Form.Group>
 
-      <Button variant="primary" type="submit"onClick={handleSubmit}>Submit</Button>
-    </Form>
+                  <Button variant="primary" type="submit"onClick={handleSubmit}>Submit</Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
+
   );
 }
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
