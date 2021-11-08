55fc5be10b55d857fe3bdc606a987f88a536827b
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-11-08 11:24:17 +0100

    Update movie-card.jsx
    for some reason I had to overwrite or compare the main-view and after overwrite, the movie-card disappeared.
    Now reloading it into the 3.6 branch

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,27 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
+
   render() {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Header>
+          <Card.Title> {movie.Title}</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>          
+            Description: {movie.Description};
+            Director: {movie.Director.Name};
+            Bio: {movie.Director.Bio};
+            Genre: {movie.Genre};
+            Description: {movie.Genre.Description}
+          </Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
 
+
+
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
-  }).isRequired,
-  onClick: PropTypes.func.isRequired
+    ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birthday: PropTypes.instanceOf(Date).isRequired,
+      Deathday: PropTypes.instanceOf(Date)
+    }),
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    })
+  }).isRequired
 };
-
-
-
 
 
 
