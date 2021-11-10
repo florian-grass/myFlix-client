82e516c1fc50e4d52465101ab7c2361c5cb22ba6
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-11-10 09:37:38 +0100

    Update movie-card.jsx
    imported axios

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
+import axios from 'axios';
 
 import { Link } from 'react-router-dom';
 
