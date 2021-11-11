475974f43c67e4651b8315a2ba04216007732df4
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-11-11 22:02:24 +0100

    Update login-view.jsx
    updated PropTypes
    updated axios.post url

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { Container, Row, Col, CardGroup, Card, Form, Button, Navbar, Nav } from 'react-bootstrap';
 
 import './login-view.scss';
+import logo from '../img/myFlix-logo.jpg';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -11,13 +12,14 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://stark-chamber-97082.herokuapp.com/', {
+    // send a request to the server for authentication
+    axios.post('https://stark-chamber-97082.herokuapp.com/login', {
       Username: username,
       Password: password,
     })
       .then(response => {
         const data = response.data;
-        console.log(data);
+        props.onLoggedIn(data);
         window.open("/" , "_self");
         // The second argument "_self" is necessary, so that
         // the page will open in the current page
@@ -30,11 +32,11 @@ export function LoginView(props) {
 
   return (
     <Container>
-      {/* <Navbar bg="dark" variant="dark" fixed="top">
+      <Navbar variant="dark" fixed="top" expand="lg">
         <Container>
           <Navbar.Brand href="#home">
             <img
-              src="../img/myFlix-logo.jpg"
+              src={logo}
               width="250"
               height="80"
               className="d-inline-block align-top"
@@ -44,14 +46,12 @@ export function LoginView(props) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#login">Login</Nav.Link>
-              <Nav.Link href="#movies">Movies</Nav.Link>
+              <Nav.Link href="/register">Registration</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Container> */}
+      <Container>
         <Row className="justify-content-md-center">
           <Col sm={12} md={10} lg={8} xl={6}>
             <CardGroup>
@@ -81,26 +81,22 @@ export function LoginView(props) {
                       />
                     </Form.Group>
 
-                    <Button /*className="primary-button"*/ variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+                    <Button className="primary-button" variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
                   </Form>
                 </Card.Body>
               </Card>
             </CardGroup>
           </Col>
         </Row>
-      {/* </Container> */}
+      </Container>
     </Container>
 
   );
 }
 
-// LoginView.propTypes = {
-//   user: PropTypes.shape({
-//     Username: PropTypes.string.isRequired,
-//     Password: PropTypes.string.isRequired,
-//   }).isRequired
-// };
-
-LoginView.propTypes = {
-  onLoggedIn: PropTypes.func.isRequired
+LoginView.PropTypes = {
+  user: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+  }).isRequired
 };
